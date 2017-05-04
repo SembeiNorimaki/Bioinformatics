af4674ae8c19d77ffdbeddd8d098919254123b14
SembeiNorimaki <alonso.isaac@gmail.com>
2017-05-04 19:47:02 +0200

    EM algorithm working

--- Bio_V/EM.py
+++ Bio_V/EM.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 def read_file():
-    filename = 'EM.in1'
+    filename = 'tests/EM.in3'
     with open(filename,'r') as fin:
-        fin.readline()  #input
+        fin.readline()  # TODO: handle first line better
         k,m = map(int,fin.readline().rstrip().split())
         beta = float(fin.readline().rstrip())
         data = []
@@ -24,28 +25,29 @@ def em(k,m,beta,data):
     # select the first k points from Data as the first centers
     centers = data[0:k,:]
 
-    # E-step
-    # Centers to Soft Clusters (E-step)
-    # (Data, ?, Parameters) -> HiddenMatrix
     for aa in range(100):
+        # E-step
+        # Centers to Soft Clusters (E-step)
+        # (Data, ?, Parameters) -> HiddenMatrix
         HiddenMatrix = np.zeros((npoints,k))
         for idx, data_i in enumerate(data):
             d = np.sqrt(np.sum(np.power(data_i-centers,2),1))
             d = np.exp(-beta*d)
             HiddenMatrix[idx,:] = np.array(d.T) / np.sum(d)
 
-        print HiddenMatrix
         # M-step
         # Soft Clusters to Centers (M-step)
         #(Data, HiddenMatrix, ?) -> Parameters
         centers = np.zeros((k,m))
         for i in range(HiddenMatrix.shape[1]):
-            # is this operation * a dot product?
-            centers[i,:] = HiddenMatrix[:,i] * data
-    print centers
+            centers[i,:] = (HiddenMatrix[:,i] * data) / np.sum(HiddenMatrix[:,i])
+            
+    return centers
     
 
-    
-k,m,beta,data = read_file()
-em(k,m,beta,data)
+if __name__ == '__main__':    
+    k,m,beta,data = read_file()
+    centers = em(k,m,beta,data)
+    for i in centers:
+        print ' '.join(map(str, i))
 
